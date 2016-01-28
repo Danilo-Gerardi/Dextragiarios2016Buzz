492ab3f1747c41c0866590a216257c3eae53e12f
Fabio-Ricci <fabioricci9@gmail.com>
2016-01-28 10:06:50 -0200

    Atv7

--- atividades/src/main/java/br/com/dextra/estagio2015/atv07/Comparator.java
+++ atividades/src/main/java/br/com/dextra/estagio2015/atv07/Comparator.java
@@ -3,21 +3,18 @@ package br.com.dextra.estagio2015.atv07;
 public class Comparator {
 
 	public boolean comparaStrings (String...strings) {
-		for (int i = 0; i < strings.length; i++ ){
+		for (int i = 0; i < strings.length; i++){
 			strings[i] = strings[i].toUpperCase();
 		}
-		boolean correto = true;
+		
 		for (String frase : strings) {
 			for (int i = 0; i < strings.length; i++) {
-				if (frase != strings[i]) {
+				if (!frase.equals(strings[i])) {
 					return false;
 				}
 			}
-		}
+		}			
 		
-		
-		return correto;
-		
+		return true;	
 	}
-	
 }
