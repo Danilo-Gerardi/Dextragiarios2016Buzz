eff24e9d44590c6f93c79306c463b24eba5b3428
jwnx <jinx_@live.com>
2016-01-28 18:58:35 -0200

    teste 15: pagamento comentado

--- atividades/src/main/java/br/com/dextra/estagio2015/atv15/Atividade15.java
+++ atividades/src/main/java/br/com/dextra/estagio2015/atv15/Atividade15.java
@@ -3,7 +3,9 @@ package br.com.dextra.estagio2015.atv15;
 import java.math.BigDecimal;
 
 	/*
-	 * Estamos trabalhando com um sistema que possui diversas transacoes monetarias.
+	 * Estamos trabalhando com um sistema que possui diversas transacoes monetarias. 
+	 * 
+	 * R: O problema aqui era criar um teste pro metodo subtract do Pagamento :)
 	 */
 public class Atividade15 {
 
