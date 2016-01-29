3542bff5193b65fecbcddbe52f52e32a61285dea
Thaissa Candella <thaissa.candella@dls241.lab.dextra.com.br>
2016-01-29 15:28:57 -0200

    ATividade 6 atualizada

--- atividades/src/test/java/br/com/dextra/estagio2015/atv06/Atividade6Test.java
+++ atividades/src/test/java/br/com/dextra/estagio2015/atv06/Atividade6Test.java
@@ -1,14 +1,23 @@
 package br.com.dextra.estagio2015.atv06;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import br.com.dextra.estagio2015.atv10.CintoDoElvis;
+import br.com.dextra.estagio2015.atv13.Venda;
 import br.com.dextra.estagio2015.comum.Banco;
 import br.com.dextra.estagio2015.comum.JPAUtils;
 import br.com.dextra.estagio2015.comum.JPA_DB_Utils;
+
+
 
 public class Atividade6Test {
 
@@ -21,19 +30,25 @@ public class Atividade6Test {
 	// que consiste no acréscimo de uma anotation para o auto-incremento da variável id no bd
 	
 	@Test
-	public void testIdBanco() {
+	public void testeIdBanco() {
 		
 		List<Banco> bancos = new ArrayList<Banco>();
 
 		bancos.add(criaBanco("A"));
 		bancos.add(criaBanco("C"));
-		bancos.add(criaBanco("E"));
-		bancos.add(criaBanco("G"));
+		//bancos.add(criaBanco("E"));
+		//bancos.add(criaBanco("G"));
 		
-		System.out.println("Bancos");
-		for (Banco b : bancos)
-			System.out.println(b.getId());
+		//System.out.println("Bancos");
 		
+		//for (Banco b : bancos)
+		//	System.out.println(b.getId());
+		
+			
+		EntityManager em = JPAUtils.getEM();	
+		
+		//assertEquals(2,  em.createQuery("FROM Banco", Banco.class).getResultList().size());
+		//assertEquals(1L,  em.createQuery("FROM Banco", Banco.class).getResultList().get(0).getId());
 	}
 	
 	private Banco criaBanco(String nome) {
