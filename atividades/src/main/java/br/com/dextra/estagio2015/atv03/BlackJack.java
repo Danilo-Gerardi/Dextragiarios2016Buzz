3dcd298428a6ae4a25566a923de5779842826e17
Lucas <lucas.cantelli@dextra-sw.com>
2016-02-01 08:48:01 -0200

    Refatorando ATV3

--- atividades/src/main/java/br/com/dextra/estagio2015/atv03/BlackJack.java
+++ atividades/src/main/java/br/com/dextra/estagio2015/atv03/BlackJack.java
@@ -3,11 +3,12 @@ package br.com.dextra.estagio2015.atv03;
 public class BlackJack {
 
 	public static void main(String[] args) {
-		Jogo jogo = new Jogo();
 		Jogador jogador = new Jogador();
 		Jogador computador = new Jogador();
+		Jogo jogo = new Jogo(jogador, computador);
 
-		jogo.comeca(jogador, computador);
+
+		jogo.comeca();
 
 	}
 }
